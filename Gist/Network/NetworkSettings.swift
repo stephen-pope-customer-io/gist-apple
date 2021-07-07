75b50a2ffac843db40a00fbc36be287b9d3018e4
Bernard Gatt <bernard@bourbon.sh>
2021-07-07 16:05:54 +0200

    Removed unused endpoint

--- Gist/Network/NetworkSettings.swift
+++ Gist/Network/NetworkSettings.swift
@@ -2,7 +2,6 @@ protocol NetworkSettings {
     var analyticsAPI: String { get }
     var queueAPI: String { get }
     var gistAPI: String { get }
-    var gist: String { get }
     var renderer: String { get }
 }
 
@@ -10,7 +9,6 @@ struct NetworkSettingsProduction: NetworkSettings {
     let analyticsAPI = "https://analytics.api.gist.build"
     let queueAPI = "https://queue.api.gist.build"
     let gistAPI = "https://api.gist.build"
-    let gist = "https://app.gist.build"
     let renderer = "https://renderer.gist.build/1.0"
 }
 
@@ -18,7 +16,6 @@ struct NetworkSettingsDevelopment: NetworkSettings {
     let analyticsAPI = "https://analytics.api.dev.gist.build"
     let queueAPI = "https://queue.api.dev.gist.build"
     let gistAPI = "https://api.dev.gist.build"
-    let gist = "https://app.dev.gist.build"
     let renderer = "https://renderer.gist.build/1.0"
 }
 
@@ -26,6 +23,5 @@ struct NetworkSettingsLocal: NetworkSettings {
     let analyticsAPI = "http://analytics.api.local.gist.build:88"
     let queueAPI = "http://queue.api.local.gist.build:86"
     let gistAPI = "http://api.local.gist.build:83"
-    let gist = "http://app.local.gist.build:8000"
     let renderer = "http://app.local.gist.build:8080/web"
 }
